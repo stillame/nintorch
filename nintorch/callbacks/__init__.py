445a6296746e35ba42bd56c30ad60a2c029be83a
ninnart-fuengfusin <ninnart.fuengfusin@yahoo.com>
2020-07-13 20:29:16 +0900

    Add callbacks

---
+++ nintorch/callbacks/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" Collection of callback functions designed for nintorch trainer.
+"""
+
+class CallBack(object):
+    def __init__():
+        return
+
+class EarlyExit(object):
+    def ___init__(self, , delta: float) -> None:
+        return
+    
